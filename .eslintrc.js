8d6e960b943a1e2bf7f2daca13d677c5c0da991a
paridhi <paridhi815@gmail.com>
2018-01-29 23:59:24 +0530

    completed 1st exercise

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+      "no-console": 0,
+      "no-undef": 0,
+      'prefer-const': 0,
+      "one-var":0,
+      "prefer-arrow-callback":0,
+      "func-names":0,
+      "no-param-reassign":0
+    }
+};
