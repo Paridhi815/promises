79895b3ace6276030f56f7ef203695a21a130cc7
paridhi <paridhi815@gmail.com>
2018-01-31 11:48:55 +0530

    #TC1: check if iterate returns a number

---
+++ ex10/10_an_important_rule.js
@@ -0,0 +1,5 @@
+function iterate(num) {
+  console.log(num);
+  return num + 1;
+}
+module.exports = iterate;
--- ex10/10_an_important_rule.test.js
+++ ex10/10_an_important_rule.test.js
@@ -1,10 +1,7 @@
 let tester = require('./10_an_important_rule');
 
 describe('Return value check:', () => {
-  it('Check if it alwaysThrows function throws an error a:', () => {
-    expect(promise).toBeInstanceOf(Promise);
-  });
   it('Check if it iterate returna argument +1:', () => {
-    expect(promise).toBeInstanceOf(Promise);
+    expect(typeof tester(5)).toBe('number');
   });
 });
