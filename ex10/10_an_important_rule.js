6599692b711013e191d9790b86f0985ec7eae9fe
paridhi <paridhi815@gmail.com>
2018-01-31 11:52:39 +0530

    #TC2: check if alwaysThrows throws an error 'on hoes'

--- ex10/10_an_important_rule.js
+++ ex10/10_an_important_rule.js
@@ -2,4 +2,12 @@ function iterate(num) {
   console.log(num);
   return num + 1;
 }
-module.exports = iterate;
+
+function alwaysThrows() {
+  throw new Error('OH NOES');
+}
+
+module.exports = {
+  iterate,
+  alwaysThrows,
+};
--- ex10/10_an_important_rule.test.js
+++ ex10/10_an_important_rule.test.js
@@ -2,6 +2,10 @@ let tester = require('./10_an_important_rule');
 
 describe('Return value check:', () => {
   it('Check if it iterate returna argument +1:', () => {
-    expect(typeof tester(5)).toBe('number');
+    expect(typeof tester.iterate(5)).toBe('number');
+  });
+
+  it('Check if it alwaysThrows function throws an error a:', () => {
+    expect(tester.alwaysThrows).toThrow('OH NOES');
   });
 });
